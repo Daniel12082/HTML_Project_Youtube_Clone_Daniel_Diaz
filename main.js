7a92acfe44cafbcd8ef0b5537cd343a389f79a1a
Daniel12082 <137439716+Daniel12082@users.noreply.github.com>
2023-08-26 23:48:25 -0500

    feat :sparkles: Player Add video playback page

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var menuIcon = document.querySelector(".ri-menu-line")
-var sidebar = document.querySelector(".sidebar") 
+var sidebar = document.querySelector(".sidebar")
+var content = document.querySelector(".content")
 
 menuIcon.onclick = function(){
     sidebar.classList.toggle("small-sidebar");
+    content.classList.toggle("small-content")
 }

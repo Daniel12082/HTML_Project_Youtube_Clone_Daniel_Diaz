fe83bb99e7b4be3d48dd2007605ed42c2bbed5e4
daniel12082 <137439716+Daniel12082@users.noreply.github.com>
2023-08-28 13:26:32 -0500

    feat :sparkles: chanel add local json file with channel details and DOM modification

--- js/principal.js
+++ js/principal.js
@@ -1,9 +1,24 @@
-var menuIcon = document.querySelector(".menu-icon"); 
-var sidebar = document.querySelector(".sidebar");    
-var container = document.querySelector(".container"); 
-
-menuIcon.onclick = function() { 
-    sidebar.classList.toggle("small-sidebar");
-    container.classList.toggle("large-container");
+const path = "videos"
+let video =async()=>{
+    let videos = await fetch(`../json/videos.json`);
+    let channel = await fetch(`../json/channel.json`);
+    let vid = await videos.json();
+    let cha = await channel.json()
+    let Selection = document.querySelector("#video");
+    Selection.insertAdjacentHTML("beforeend",/*html*/ `
+        ${vid.contents.map((value)=>/*html*/` 
+        <div class="vid-list">
+            <a href="start.html"><img src="${value.video.thumbnails[3].url}" class="thumbnai1"></a>
+            <div class="flex-div">
+                <img src="${cha.avatar[2].url}">
+                <div class="vid-info">
+                    <a href="start.html">${value.video.title}</a>
+                    <p>${cha.title}</p>
+                    <p>${value.video.stats.views} views &bull; ${value.video.publishedTimeText}</p>
+                </div>
+            </div>
+        </div>`).join(" ")}
+    `)
 }
-
+video()
+console.log(vid())

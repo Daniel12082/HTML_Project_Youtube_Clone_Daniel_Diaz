8957cac172c08254635d17c612fb8b0b8cd33bda
Daniel12082 <137439716+Daniel12082@users.noreply.github.com>
2023-08-28 18:23:25 -0500

    feat :sparkles: banner change of the banner to the required banner

--- js/principal.js
+++ js/principal.js
@@ -1,9 +1,9 @@
-const path = "videos"
+
 let video =async()=>{
     let videos = await fetch(`../json/videos.json`);
     let channel = await fetch(`../json/channel.json`);
     let vid = await videos.json();
-    let cha = await channel.json()
+    let cha = await channel.json();
     let Selection = document.querySelector("#video");
     Selection.insertAdjacentHTML("beforeend",/*html*/ `
         ${vid.contents.map((value)=>/*html*/` 
@@ -19,6 +19,31 @@ let video =async()=>{
             </div>
         </div>`).join(" ")}
     `)
+let Selection2 = document.querySelector("#video")
+Selection.insertAdjacentHTML("beforeend",/*html*/ `
+<div class="banner">
+        <img src="" alt="" id=" banner">
+    </div>
+`)
+}
+
+let sidebar = async()=>{
+    let sidebarpos = await fetch(`../json/config.json`);
+    let sid = await sidebarpos.json();
+    let selection3 = document.querySelector("#sidebar")
+    selection3.insertAdjacentHTML  ("beforeend", /*html*/ `
+    <div class="shortcut-links">
+    <a href="/index.html"><img src="${sid.sidebar[]}"><p>Home</p></a>
+    ${sid.sidebar.map((value)`
+    `)}
+            
+            <a href=""><img src="img/explore.png"><p>Explore</p></a>
+            <a href=""><img src="img/subscription.png"><p>Subscription</p></a>
+            <a href=""><img src="img/library.png"><p>Library</p></a>
+            <a href=""><img src="img/history.png"><p>History</p></a>
+            <a href=""><img src="img/playlist.png"><p>Playlist</p></a>
+            <a href=""><img src="img/messages.png"><p>Messages</p></a>
+            <a href=""><img src="img/show-more.png"><p>show More</p></a>
+            <hr>`)
 }
 video()
-console.log(vid())
